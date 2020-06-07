1d8e4cf6fe7a36008a74949afc6a4e25de53e62d
Fabio Kawasaki <binhu_kawa@hotmail.com>
2020-06-07 19:39:18 -0300

    Aula 9

---
+++ Aula09/src/app/professores-angular-cli/professores-angular-cli.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-professores-angular-cli',
+  templateUrl: './professores-angular-cli.component.html',
+  styleUrls: ['./professores-angular-cli.component.css']
+})
+export class ProfessoresAngularCliComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
